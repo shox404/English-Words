45faecdc38b627b3119fc4ed8693c7439639146f
shox404 <shxxx404@gmail.com>
2024-09-25 00:04:19 +0500

    Speaker

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect } from "react";
 import { useWords } from "./_store/words";
+import { Card, CardBody, CardHeader } from "@nextui-org/react";
 
 export default function Home() {
   const { words, get } = useWords();
@@ -10,5 +11,37 @@ export default function Home() {
     if (words.length == 0) get();
   }, [words]);
 
-  return <main>{}</main>;
+  const speech = (text: string) => {
+    const uttr = new SpeechSynthesisUtterance(text);
+    const voices = globalThis.speechSynthesis.getVoices();
+    const slVc = voices.find((voice) => voice.name === "Google US English");
+    if (slVc) {
+      uttr.voice = slVc;
+      globalThis.speechSynthesis.speak(uttr);
+    }
+  };
+
+  return (
+    <main>
+      {words.map((item, index) => (
+        <Card key={index} className="card">
+          <CardHeader className="flex justify-between items-center">
+            <div className="flex gap-3 items-center">
+              <h1 className="capitalize">{item.word}</h1> -
+              <h2>{item.translate}</h2>
+            </div>
+            <button
+              className="bg-black p-1 rounded-md"
+              onClick={() => speech(item.word)}
+            >
+              🔊
+            </button>
+          </CardHeader>
+          <CardBody>
+            <p className="capitalize">{item.example}</p>
+          </CardBody>
+        </Card>
+      ))}
+    </main>
+  );
 }
